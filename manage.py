6d9cd023c4536f47b27426f4c812a128faff5fa8
RainChen <hirainchen@gmail.com>
2020-09-09 22:50:30 +0800

    add cli command: flask create-db

--- manage.py
+++ manage.py
@@ -7,11 +7,21 @@ from api.datab import db
 from config import config
 from main import create_app
 
+from sqlalchemy_utils import create_database 
 
 config_name = getenv("FLASK_ENV", "production")
 app = create_app(config.get(config_name))
 migrate = Migrate(app, db)
 manager = Manager(app)
+
+# add cli command: flask create-db
+@app.cli.command()
+def create_db():
+    """Create the configured database."""
+    db_uri = config.get(config_name).SQLALCHEMY_DATABASE_URI
+    db_name = db.engine.url.database
+    print("create db: ", db_name)
+    create_database(db_uri)
 
 if "__main__ " == __name__:
     manager.run()
