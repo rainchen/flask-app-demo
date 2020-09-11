489c55901715744d6b229d765ab94a44a48e1a9c
RainChen <hirainchen@gmail.com>
2020-09-11 18:19:02 +0800

    add BaseModel

--- api/models/user.py
+++ api/models/user.py
@@ -1,12 +1,12 @@
+from api.models.base_model import BaseModel
 from api.datab import db
 
-
-class User(db.Model):
+class User(BaseModel):
     __tablename__ = "users"
 
     id = db.Column(db.String(80), primary_key=True, default="234")
-    first_name = db.Column(db.String, nullable=False)
-    last_name = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String(length=50), nullable=False)
+    last_name = db.Column(db.String(length=50), nullable=False)
     username = db.Column(db.String(100), unique=True)
 
     def __repr__(self):
